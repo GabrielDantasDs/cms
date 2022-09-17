b6e7802d175898de455b640acdc967d107a44a0b
Gabriel <gabri4k@gmail.com>
2022-09-17 11:04:57 -0300

    create resource users

--- assistente-virtual-backend/app/src/app.module.ts
+++ assistente-virtual-backend/app/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PostModule, PrismaModule],
+  imports: [PostModule, PrismaModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ assistente-virtual-backend/app/src/user/dto/create-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsString, MaxLength, MinLength, Matches
+ } from "class-validator";
+
+export class CreateUserDto {
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'password too weak'
+    })
+    password: string;
+
+    @IsString()
+    name: string;
+}
